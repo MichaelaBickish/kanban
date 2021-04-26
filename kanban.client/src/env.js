4472184c51ca873f5b539ff1353c6c1e7dc072b0
Melia88 <castro.melissa8.com>
2021-04-26 13:55:23 -0600

    .env is set up

--- kanban.client/src/env.js
+++ kanban.client/src/env.js
@@ -1,5 +1,8 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
-export const domain = ''
-export const audience = ''
-export const clientId = ''
+export const domain = 'melia88.us.auth0.com'
+// domain comes from  Applications Applications click on the one I used the client ID of and copy domain
+export const audience = 'https://todo-api.com'
+// audience is applications API todo-api
+export const clientId = 'D7UVniD8W6ZCk4tmSCNfvRDBq6fwncLX'
+// client ID comes from  Applications Applications
