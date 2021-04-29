90dac65974e6af4929d123a2f23cec9961c25d0b
Melia88 <castro.melissa8.com>
2021-04-28 18:02:38 -0600

    comments started

--- kanban.client/src/components/TaskComponent.vue
+++ kanban.client/src/components/TaskComponent.vue
@@ -1,7 +1,48 @@
 <template>
   <div class="task-component shadow rounded m-3 ">
     <!-- this is injected into the list component -->
-    {{ task.title }}
+
+    <div class="card">
+      <div class="card-body">
+        {{ task.title }}
+
+        <!-- btn -->
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle"
+                  type="button"
+                  id="dropdownMenu2"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+          >
+            Comment
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <!-- start -->
+            <div class="input-group mb-3">
+              <input type="text" class="form-control" placeholder="Add Comment..." aria-describedby="basic-addon2">
+              <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="button">
+                  +
+                </button>
+              </div>
+            </div>
+            <!-- end -->
+
+            <li class="dropdown-item">
+              Action
+            </li>
+            <li class="dropdown-item">
+              Another action
+            </li>
+            <li class="dropdown-item">
+              Something else here
+            </li>
+          </div>
+        </div>
+      <!-- btn -->
+      </div>
+    </div>
   </div>
 </template>
 
